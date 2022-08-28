880088c5a2e6ae07fadcc4bedf7f6c9a0f03901a
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-08-28 14:13:35 +0300

    Add main block

--- source/js/toggle-menu.js
+++ source/js/toggle-menu.js
@@ -1,6 +1,17 @@
 const toggleMenu = document.querySelector('.header__toggle-menu');
+const mainNav = document.querySelector('.main-nav--open');
 
+
+mainNav.classList.remove('main-nav--open');
 
 toggleMenu.addEventListener('click', function() {
   toggleMenu.classList.toggle('open')
+})
+
+toggleMenu.addEventListener('click', function() {
+  if(toggleMenu.classList.contains('open')) {
+    mainNav.classList.toggle('main-nav--open')
+  }else {
+    mainNav.classList.remove('main-nav--open')
+  }
 })

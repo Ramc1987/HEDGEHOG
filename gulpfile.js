34f001c63e3a8db875e4d8ce138618365bfbfa57
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-09-03 14:20:01 +0300

    Finish footer

--- gulpfile.js
+++ gulpfile.js
@@ -127,6 +127,7 @@ const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/js/*.js', gulp.series(scripts));
   gulp.watch('source/*.html', gulp.series(html,reload));
+  gulp.watch('source/img/**/*.svg', gulp.series(html,reload));
 }
 
 //Build

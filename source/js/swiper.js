2087db7f00139b4f2341836b052fdcade36e76b4
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-09-03 12:13:12 +0300

    Finish main

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -1,9 +1,29 @@
-var swiper = new Swiper(".mySwiper", {
+new Swiper(".mySwiper", {
     slidesPerView: "auto",
     centeredSlides: true,
     spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
     },
+
+    slidesPerView: 1.5,
+
+    // loop: true,
+
+    // autoplay: {
+    //   delay: 1500,
+    // },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1.5,
+      },
+
+      680: {
+        slidesPerView: 2,
+      }
+
+    }
+
 });

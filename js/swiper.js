b049d7a4e62deb42869cbaac30d388d12fd1fbfa
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-09-03 18:30:04 +0300

    Updates

---
+++ js/swiper.js
@@ -0,0 +1,1 @@
+new Swiper(".mySwiper",{slidesPerView:"auto",centeredSlides:!0,spaceBetween:30,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},slidesPerView:1.5,breakpoints:{320:{slidesPerView:1.5},680:{slidesPerView:2}}});

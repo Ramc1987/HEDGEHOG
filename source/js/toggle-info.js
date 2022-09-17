999a96ee21894480129e911b945f785d705aa235
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-09-17 17:50:52 +0300

    Fix bag

--- source/js/toggle-info.js
+++ source/js/toggle-info.js
@@ -1,8 +1,10 @@
-const toggleBtn = document.querySelector('.slider__slide-info-btn');
+const toggleBtn = document.querySelectorAll('.slider__slide-info-btn');
 const navbarCard = document.querySelector('.slider__slide-navbar');
 
 function infoBtn() {
-  toggleBtn.addEventListener('click', function() {
-    navbarCard.style.transform = scale(1);
+  toggleBtn.forEach((item) => {
+    item.addEventListener('click', function() {
+    navbarCard.style.transform = 'scale(1)';
+  })
   })
 }

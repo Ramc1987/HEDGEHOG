3bdb6e8c000119c839f578c124a79ea620c27b52
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-09-22 02:15:11 +0300

    add counter in basket

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,13 +1,18 @@
-const catalogBtn = document.querySelector('.main-nav__item-link--catalog')
+const catalogBtn = document.querySelector('.main-nav__item-link--catalog');
+const itemHover = document.querySelector('.main-nav__item-hover');
 const popupList = document.querySelector('.popup__list');
 
 
 function openPopup() {
   if(popupList.classList.toggle('popup__list--close')) {
-    popupList.classList.remove('popup__list--open')
+    itemHover.style.transform='rotate(0deg)';
+    itemHover.style.left='-9px';
+    popupList.classList.remove('popup__list--open');
   }else{
+    itemHover.style.transform='rotate(90deg)';
+    itemHover.style.left='-13px';
     popupList.classList.add('popup__list--open')
   }
-}
+};
 
 catalogBtn.addEventListener('click', openPopup);
